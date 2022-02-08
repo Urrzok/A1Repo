5e5fa6edd6f9a88ab0bfa1b0d97311dcdd908c71
Alex Kindler <Assassinshy@gmail.com>
2022-02-07 18:55:10 -0600

    Initial commit

---
+++ ConsoleApplication23/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System .Threading;
+
+namespace ConsoleApplication23
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var x = 0;
+            var y = 1;
+
+            try
+            {
+                var z = y / x;
+                Console.WriteLine(z);
+            }
+            
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw new ArgumentException("Sup bro");
+            }
+        }
+    }
+}
