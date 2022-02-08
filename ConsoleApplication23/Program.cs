3f42339b24eb526d614c1d4ecf56334d11704df1
Alex Kindler <91154262+Urrzok@users.noreply.github.com>
2022-02-07 21:29:07 -0600

    Update Program.cs

--- ConsoleApplication23/Program.cs
+++ ConsoleApplication23/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+using System;
 using System .Threading;
+using System.IO;
+using System.Runtime.InteropServices;
 
 namespace ConsoleApplication23
 {
@@ -7,20 +9,74 @@ namespace ConsoleApplication23
     {
         public static void Main(string[] args)
         {
-            var x = 0;
-            var y = 1;
+            string file = "ticket.csv";
+            string userChoice;
 
-            try
+            do
             {
-                var z = y / x;
-                Console.WriteLine(z);
-            }
-            
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw new ArgumentException("Sup bro");
-            }
+                Console.WriteLine("\n1: Read data from file");
+                Console.WriteLine("2: Create file from data");
+                Console.WriteLine("Any other key will exit the program");
+
+                userChoice = Console.ReadLine();
+
+                if (userChoice == "1")
+                {
+
+                    if (File.Exists(file))
+                    {
+                        
+                        StreamReader boop = new StreamReader(file);
+                        while (!boop.EndOfStream)
+                        {
+
+
+                            string line = boop.ReadLine();
+
+                            string[] arr = line.Split('|');
+                            Console.Write("{0},{1},{2},{3},{4},{5},{6}|{6}|{6}",arr[0],arr[1],arr[2],arr [3],arr [4], arr[5],arr[6]);
+                            
+                        }
+                        boop.Close();
+                    }else {
+                        Console.WriteLine("File does not exist");
+                    }
+
+                }
+                
+                else if (userChoice == "2")
+                {
+                    StreamWriter beep = new StreamWriter(file);
+                    
+                    
+
+                    int ticketID = 1;
+                    Console.WriteLine("Give a brief description of the issue you're having");
+                    string issue = Console.ReadLine();
+
+                    String status = "Open";
+                    
+                    Console.WriteLine("Enter proper priority level");
+                    string priority = Console.ReadLine();
+                    
+                    Console.WriteLine("Enter your name");
+                    string submitter = Console.ReadLine();
+
+                    String Assigned = "Vergil Klief,John Frank,Dante Deede";
+                    string watchingChoice;
+                    do
+                    {
+                        Console.WriteLine("Enter the name of one person who is watching the ticket");
+                        string watcher = Console.ReadLine();
+
+                        Console.WriteLine("Do you want to enter another? Y/N");
+                        watchingChoice = Console.ReadLine();
+                        beep.Write("{0}|{1}|{2}|{3}|{4}|{5}|{6}", ticketID, issue, status, priority, submitter, Assigned, watcher);
+                    } while (watchingChoice != "N");
+                    
+                    beep.Close();
+                }
+            } while (userChoice == "1" || userChoice == "2");
         }
     }
 }
